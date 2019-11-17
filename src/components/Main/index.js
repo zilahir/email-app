742df4a8ce529a8abe2e2a1ba9128c5abafb752f
Richard Zilahi <richard.zilahi@salesframe.com>
2019-11-17 18:19:10 +0200

    grid layout implemented

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -3,20 +3,18 @@ import { Grid, Box } from 'grommet'
 
 const Main = () => (
 	<Grid
-		rows={['xxsmall', 'large']}
-		columns={['flex', 'flex']}
-		gap="small"
 		areas={[
-			{ name: 'header', start: [0, 0], end: [1, 0] },
-			{ name: 'sidenav', start: [0, 1], end: [0, 1] },
-			{ name: 'emailview', start: [0, 1], end: [0, 1] },
-			{ name: 'emailnav', start: [1, 1], end: [1, 1] },
+			{ name: 'nav', start: [0, 0], end: [0, 0] },
+			{ name: 'main', start: [1, 0], end: [1, 0] },
+			{ name: 'side', start: [2, 0], end: [2, 0] },
 		]}
+		columns={['small', 'medium', 'flex']}
+		rows={['calc(100vh -  15px)']}
+		gap="small"
 	>
-		<Box gridArea="header" background="brand" />
-		<Box gridArea="sidenav" background="yellow" />
-		<Box gridArea="emailnav" background="red" />
-		<Box gridArea="emailview" background="blue" />
+		<Box gridArea="nav" background="brand" />
+		<Box gridArea="main" background="brand" />
+		<Box gridArea="side" background="brand" />
 	</Grid>
 )
 
