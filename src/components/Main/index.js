c7e36a49ae0e227185a56c45c29635121d213a26
Richard Zilahi <richard.zilahi@salesframe.com>
2019-11-17 17:48:46 +0200

    implementing grid using grommet

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -4,17 +4,19 @@ import { Grid, Box } from 'grommet'
 const Main = () => (
 	<Grid
 		rows={['xxsmall', 'large']}
-		columns={['large', 'flex']}
+		columns={['flex', 'flex']}
 		gap="small"
 		areas={[
 			{ name: 'header', start: [0, 0], end: [1, 0] },
-			{ name: 'nav', start: [0, 1], end: [0, 1] },
-			{ name: 'main', start: [1, 1], end: [1, 1] },
+			{ name: 'sidenav', start: [0, 1], end: [0, 1] },
+			{ name: 'emailview', start: [0, 1], end: [0, 1] },
+			{ name: 'emailnav', start: [1, 1], end: [1, 1] },
 		]}
 	>
 		<Box gridArea="header" background="brand" />
-		<Box gridArea="nav" background="light-5" />
-		<Box gridArea="main" background="light-2" />
+		<Box gridArea="sidenav" background="yellow" />
+		<Box gridArea="emailnav" background="red" />
+		<Box gridArea="emailview" background="blue" />
 	</Grid>
 )
 
