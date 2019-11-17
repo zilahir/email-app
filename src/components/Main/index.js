b1c97848a903a6443b6aa44ca778e9717c72a0df
Richard Zilahi <richard.zilahi@salesframe.com>
2019-11-17 17:26:59 +0200

    implementing grid using grommet

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,12 +1,21 @@
 import React from 'react'
-
+import { Grid, Box } from 'grommet'
 
 const Main = () => (
-	<div>
-		<p>
-			Main
-		</p>
-	</div>
+	<Grid
+		rows={['xxsmall', 'large']}
+		columns={['large', 'flex']}
+		gap="small"
+		areas={[
+			{ name: 'header', start: [0, 0], end: [1, 0] },
+			{ name: 'nav', start: [0, 1], end: [0, 1] },
+			{ name: 'main', start: [1, 1], end: [1, 1] },
+		]}
+	>
+		<Box gridArea="header" background="brand" />
+		<Box gridArea="nav" background="light-5" />
+		<Box gridArea="main" background="light-2" />
+	</Grid>
 )
 
 export default Main
